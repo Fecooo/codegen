ffbdf1eec2a0dec6167c3d0527931c94fc71f256
Fecooo <85518187+Fecooo@users.noreply.github.com>
2021-07-28 20:17:53 +0200

    kód előnézet (by Kiwii)

--- script.js
+++ script.js
@@ -1,45 +1,39 @@
 var betuk = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var szamok = "0123456789"
+var resz = ""
 
 function generate() {
 	document.getElementById("kodok").innerHTML = "";
 	for (let i = 0; i < 500; i++) {
-		let resz = "";
-	
-		for (let j = 0; j < 20; j++) {
-	
-			if (j % 5 == 0 && j != 0) {
-				resz += "-";
-			}
-
-			if (document.getElementById(j).checked) {
-				let randomBetu = Math.floor(Math.random() * betuk.length);
-				resz += betuk[randomBetu];
-			} else {
-				let randomSzam = Math.floor(Math.random() * szamok.length);
-				resz += szamok[randomSzam];
-			}
-		}
-		
+        auto();		
 		document.getElementById("kodok").innerHTML += resz + "<br>";
-	}	
+    }
 }
 
-/*for (let i = 0; i < 500; i++) {
-    let resz = "";
+function realTimeCode() {
+    document.getElementById("realTime").innerHTML = "Your codes will look this: ";
+    auto();
+    document.getElementById("realTime").innerHTML += resz;
+}
 
-    for (let j = 0; j < 20; j++) {
-
+function auto() {
+    resz = "";
+	for (let j = 0; j < 20; j++) {
         if (j % 5 == 0 && j != 0) {
             resz += "-";
         }
+        if (document.getElementById(j).checked) {
+            let randomBetu = Math.floor(Math.random() * betuk.length);
+            resz += betuk[randomBetu];
+        } else if (document.getElementById(j + 20).checked) {
+            let randomSzam = Math.floor(Math.random() * szamok.length);
+            resz += szamok[randomSzam];
+        } else {
+            resz += "_";
+        }
+    }
+}
 
-        let randomChar = Math.floor(Math.random() * karakterek.length);
-        resz += karakterek[randomChar];
-    };
-
-    document.getElementById("kodok").innerHTML += resz + tores;
-}*/
 function display() {
 	var codes = document.getElementById('kodok');
 
